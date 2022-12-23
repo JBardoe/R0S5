f726e01ba8836b89a6b7157ac5a48274e364a4e8
JBardoe <98495948+JBardoe@users.noreply.github.com>
2022-12-23 00:01:09 +0000

    Front-end

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
+#This imports the library to this python project
 import pymongo
 
-client= pymongo.MongoClient("mongodb+srv://jackbardoe:William1@r0s5.hxkyr5l.mongodb.net/?retryWrites=true&w=majority")
+#This establishes the connection to the datanase
+client1= pymongo.MongoClient("mongodb+srv://jackbardoe:William1@r0s5.hxkyr5l.mongodb.net/?retryWrites=true&w=majority")
 
-database=client["Users"]
-collection=database["Details"]
-direct={"email": "kerrybardoe@btinternet.com", "password": "password1", "fname": "Kerry", "lname": "Bardoe", "cname": "NHS"} 
-x = collection.insert_one(direct)
+#This cycles through sections of the database
+database1=client1["Users"]
+collection1=database1["Details"]
+
+#This is an example of adding a document to the database
+direct1={"email": "kerrywoods@btinternet.com", "password": "password1", "fname": "Kerry", "lname": "Woods", "cname": "NHS"} 
+collection1.insert_one(direct1)
+
