3d945a807e03d9bb9d0f8aa60bc9b743ea7822ca
JBardoe <98495948+JBardoe@users.noreply.github.com>
2023-01-26 17:01:49 +0000

    Some changes

--- website/employee.py
+++ website/employee.py
@@ -9,18 +9,29 @@ def getEmployees():
     file1.close()
     return employees
 
-def addEmployee(name, role):
+def appendEmployee(name, role):
     file1=open("C:/Users/arron/OneDrive/Documents/GitHub/IA-Website/website/information/employees.csv","a")
     file1.write(name+","+role+"\n")
     file1.close()
+    for i in range(1,len(name)+1):
+            if name[i-1:i] == " ":
+                name[i-1:i] = "_"
+                break
+    file2=open(name+".txt","w")
+    file2.close()
+
 
 employee = Blueprint('employee', __name__)
 
 @employee.route('/employeelist', methods=['GET', 'POST'])
 def employeeList():
     employees=getEmployees()
-    if request.method == "POST":
-        return render_template("elistInput.html", employees=employees)
+    if request.method == "POST" and (request.form.get('emp')!=None):
+        emp=request.form.get('emp')
+        return redirect(url_for('employee.employeeFile', emp = emp))
+    elif request.method == "POST":
+        return redirect(url_for('employee.addEmployee'))
+
     return render_template("elist.html", employees=employees)
 
 @employee.route('/addemployee', methods=['GET', 'POST'])
@@ -30,10 +41,11 @@ def addEmployee():
         name=request.form.get('empName')
         role=request.form.get('empRole')
         employees.append([name,role])
-        addEmployee(name,role)
-        return render_template("elist.html", employees=employees)
+        appendEmployee(name, role)
+        return redirect(url_for('employee.employeeList'))
     return render_template("elistInput.html", employees=employees)
 
-@employee.route('/employeefile', methods=['GET', 'POST'])
-def employeeFile():
-    return render_template("efile.html")
+
+@employee.route('/employeefile/<emp>', methods=['GET', 'POST'])
+def employeeFile(emp):
+    return render_template("efile.html", emp=emp)
--- website/models.py
+++ website/models.py
@@ -29,3 +29,70 @@ class user:
         collection1=database1["Details"]
 
         pass #add db functionality
+
+class employ:
+    name=""
+    age=0
+    dob=""
+    maxHours=0
+    empNum=""
+    remainingHoliday=0
+    rate=0
+    training=[]
+    availability=[]
+    holiday=[]
+
+    def info(self, name):
+        tempName=name
+        for i in range(1,len(tempName)+1):
+            if tempName[i-1:i] == " ":
+                tempName[i-1:i] = "_"
+                break
+        file1=open(tempName+".txt","r")
+        j=0
+        temp=[]
+        for line in file1:
+            if j==7:
+                break
+            data=line.strip().split(":")
+            temp.append(data[1])
+            j+=1
+        self.name=temp[0]
+        self.age=int(temp[1])
+        self.dob=temp[2]
+        self.maxHours=int(temp[3])
+        self.empNum=temp[4]
+        self.remainingHoliday=int(temp[5])
+        self.rate=float(temp[6])
+        t=0
+        for line in file1:
+            if t<7:
+                t+=1
+                continue
+            data=line.strip.split(",")
+            if data[0] =="endOfTraining":
+                break
+            self.training.append([data[0],data[1]])
+        p=0
+        for line in file1:
+            if p<t:
+                p+=1
+                continue
+            data=line.strip().split(",")
+            if data[0] == "endOfAvailability":
+                break
+            self.availability.append([data[0],data[1],data[2]])
+        l=0
+        for line in file1:
+            if l<p:
+                l+=1
+                continue
+            data=line.strip.split(",")
+            self.holiday.append([data[0],data[1],data[2]])
+        file1.close()
+
+    def __init__(self, name):
+        self.name=name
+        self.info(name)
+    
+    
